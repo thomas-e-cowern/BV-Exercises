81c9bbcc8b78705fa9d9461480bbd6454fb8297b
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-02-15 19:54:17 -0500

    added images to project

--- TinderFun/TinderFun/ViewController.swift
+++ TinderFun/TinderFun/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.image = UIImage.init(named: "Person1")
     }
 
 
